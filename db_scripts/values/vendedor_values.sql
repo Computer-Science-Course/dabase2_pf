ef0005c9fa0afc3260b814f2b92412f085bf483a
MateusSantosMeuBem <a.mateussm1.5@gmail.com>
2022-10-15 11:34:03 -0300

    :tada: | Added values for database

---
+++ db_scripts/values/vendedor_values.sql
@@ -0,0 +1,24 @@
+USE pf_veiculos;
+
+INSERT INTO vendedor (`num_vend`, `nome`)
+VALUES
+	(1, 'Walter Butler'),
+	(2, 'Karl Voit'),
+	(3, 'Mary Saxton'),
+	(4, 'Jodi Inman'),
+	(5, 'Helen Mashburn'),
+	(6, 'Marie Holt'),
+	(7, 'Larry Mcafee'),
+	(8, 'Mitchel Chester'),
+	(9, 'Wilbur Mack'),
+	(10, 'James Sellers'),
+	(11, 'Margaret Gismondi'),
+	(12, 'Mary Huntley'),
+	(13, 'Arturo Gray'),
+	(14, 'Lance Kittelson'),
+	(15, 'Karen Mikkelsen'),
+	(16, 'Joyce Emory'),
+	(17, 'Loretta Macias'),
+	(18, 'Nancy Fite'),
+	(19, 'Sherman Riley'),
+	(20, 'Mary Godwin');
